e3cb2e23c207d021192659d409ec69f134e6d521
SADEEL ARDAH <sadeelardah@gmail.com>
2026-01-08 20:35:48 +0200

    Create UserEntity

---
+++ X/Models/Entities/User.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+namespace X.Models.Entities;
+public class User
+{
+    public int Id { get; set; }
+    [Required, MaxLength(30)]
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+    [Required, MaxLength(100)]
+    public string Email { get; set; } = string.Empty;
+    [MaxLength(50)]
+    public string DisplayName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
